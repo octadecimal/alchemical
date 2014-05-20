ed55a9792339a7f505829a37d602de9cbea52b3a
Dylan Heyes <dylan.heyes@gamesys.us>
2014-05-19 18:13:44 -0700

    Server: Added command constants.

---
+++ server/src/alchemical/server/const/Commands.hx
@@ -0,0 +1,11 @@
+package alchemical.server.const;
+
+/**
+ * ...
+ * @author Dylan Heyes
+ */
+class Commands
+{
+	static public inline var LOGIN:Int = 2;
+	static public inline var DEFINE_WORLD:Int = 4;
+}
--- server/src/alchemical/server/io/PacketBuilder.hx
+++ server/src/alchemical/server/io/PacketBuilder.hx
@@ -1,4 +1,6 @@
 package alchemical.server.io;
+import alchemical.server.const.Commands;
+import alchemical.server.Server.Player;
 import alchemical.server.Server.World;
 import alchemical.server.io.OutPacket;
 
@@ -16,21 +18,21 @@ class PacketBuilder
 	
 	public function loginSuccess(packet:OutPacket):Void
 	{
-		packet.writeCommand(2);					// LOGIN
+		packet.writeCommand(Commands.LOGIN);
 		packet.writeBool(1);					// success
 	}
 	
 	
 	public function loginFailure(packet:OutPacket):Void
 	{
-		packet.writeCommand(2);					// LOGIN
+		packet.writeCommand(Commands.LOGIN);// LOGIN
 		packet.writeInt16(0);					// failure
 	}
 	
 	
 	public function defineWorld(packet:OutPacket, world:World):Void
 	{
-		packet.writeCommand(4);					// DEFINE_WORLD
+		packet.writeCommand(Commands.DEFINE_WORLD);
 		packet.writeInt16(world.id);			// world id
 		packet.writeString(world.name);			// world name
 		packet.writeInt16(world.width);			// world width
@@ -42,4 +44,10 @@ class PacketBuilder
 			packet.writeInt16(world.skyLayers[i]);
 		}
 	}
+	
+	
+	/*public function definePlayer(packet:OutPacket, player:Player) 
+	{
+		//packet.writeCommand
+	}*/
 }
